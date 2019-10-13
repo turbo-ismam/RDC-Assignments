b8ef89201d46a7952b088fe729e43ee495fb15f7
marco <marco.hu2@studio.unibo.it>
2019-10-13 15:02:56 +0200

    ciao

--- superserver.c
+++ superserver.c
@@ -7,30 +7,59 @@
 #include<netinet/in.h>
 #include<signal.h>
 #include<errno.h>
-
+#define FILENAME "serverlist.txt"
 //Constants and global variable declaration goes here
 
 
 //Service structure definition goes here
 typedef struct 
 {
-	char[4] TransportProtocol;	//’tcp’ if protocol is TCP, ’udp’ if protocol is UDP
-	char[7] serviceMode;		//’wait’ if service is concurrent, ‘nowait’ otherwise
-	char[4] port;			//port on which service is available
-	char[50] CompleteName;		//complete name of the service, including full path
-	char[20] Name;			//name of the service
+	char TransportProtocol[4];	//’tcp’ if protocol is TCP, ’udp’ if protocol is UDP
+	char serviceMode[7];		//’wait’ if service is concurrent, ‘nowait’ otherwise
+	char port[6];			//port on which service is available
+	char CompleteName[50];		//complete name of the service, including full path
+	char Name[20];			//name of the service
 	int SocketDescriptor;		//socket descriptor associated to the port
 	int PID;			//process ID
 } serviceInfo;
-
+struct sockaddr_in server_addr;
 //Function prototype devoted to handle the death of the son process
 void handle_signal (int sig);
 
 int  main(int argc,char **argv,char **env){ // NOTE: env is the variable to be passed, as last argument, to execle system-call
 	// Other variables declaration goes here
+	serviceInfo si[10];
+	int i=0, br, lr;
+	char ch;
 	
 	
+	FILE *fileptr;
+	if((fileptr = fopen(FILENAME, "r"))==NULL)
+	{
+			printf("errore apertura file");
+			exit(1);
+	};
+	while(fscanf(fileptr, "%s %s %s %s\n", &si[i].CompleteName, &si[i].TransportProtocol, &si[i].port, &si[i].serviceMode)==4)
+	{
+	printf("%s %s %s %s\n", si[i].CompleteName, si[i].TransportProtocol, si[i].port, si[i].serviceMode);
+	if (si[i].TransportProtocol=="tcp") 
+	{
+		si[i].SocketDescriptor = socket(AF_INET,SOCK_STREAM, IPPROTO_TCP);
+	}
+	else
+	{
+		si[i].SocketDescriptor = socket(AF_INET,SOCK_DGRAM, IPPROTO_UDP);
+	}
+	br= bind(si[i].SocketDescriptor, INADDR_ANY
+	
+	if (si[i].SocketDescriptor<0) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+	i++;
+	}
 	// Server behavior implementation goes here
+	
 	
 	
 	signal (SIGCHLD,handle_signal); /* Handle signals sent by son processes - call this function when it's ought to be */
@@ -41,7 +70,6 @@ int  main(int argc,char **argv,char **env){ // NOTE: env is the variable to be p
 // handle_signal implementation
 void handle_signal (int sig){
 	// Call to wait system-call goes here
-	
 	
 	switch (sig) {
 		case SIGCHLD : 
